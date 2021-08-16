7be1b06cf175f177c99d47c3b96cc2ba3e3aafe8
Amine Rehioui <amine@franticsoftware.com>
2021-08-16 03:12:29 +0200

    Prototyping

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,56 @@
 
 #include <iostream>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+struct Color {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+struct Context {
+	unsigned char* pixels;
+	int pixelsStride;
+	int bytesPerPixel;
+};
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+void resizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
-unsigned char* pixels;
-int stride;
-bool pixelsDirty = true;
-void setPixel(unsigned int x, unsigned int y, unsigned char r, unsigned char g, unsigned char b) {
-	const auto index = (y * stride * 3) + x * 3;
-	pixels[index + 0] = r;
-	pixels[index + 1] = g;
-	pixels[index + 2] = b;
-	pixelsDirty = true;
+void setPixel(const Context& context, unsigned int x, unsigned int y, const Color& color) {
+	const auto index = (y * context.pixelsStride * context.bytesPerPixel) + x * context.bytesPerPixel;
+	context.pixels[index + 0] = color.r;
+	context.pixels[index + 1] = color.g;
+	context.pixels[index + 2] = color.b;	
+}
+
+void line(const Context& context, int x0, int y0, int x1, int y1, const Color& color) {
+	// Based on https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	const auto dx = abs(x1 - x0);
+	const auto sx = x0 < x1 ? 1 : -1;
+	const auto dy = -abs(y1 - y0);
+	const auto sy = y0 < y1 ? 1 : -1;
+	auto err = dx + dy;
+	while (true) {
+		setPixel(context, x0, y0, color);
+		if (x0 == x1 && y0 == y1) {
+			break;
+		}
+		const auto e2 = 2 * err;
+		if (e2 >= dy) {
+			err += dy;
+			x0 += sx;
+		}
+		if (e2 <= dx) {
+			err += dx;
+			y0 += sy;
+		}
+	}
 }
 
 int main(void) {
@@ -40,8 +72,8 @@ int main(void) {
 	}
 
 	glfwMakeContextCurrent(window);
-	glfwSetKeyCallback(window, key_callback);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetFramebufferSizeCallback(window, resizeCallback);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize OpenGL context" << std::endl;
@@ -50,7 +82,7 @@ int main(void) {
 
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
+	glViewport(0, 0, width, height);	
 	glClearColor(0.f, 0.f, 0.f, 1.0f);
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
@@ -142,14 +174,11 @@ int main(void) {
 	glEnableVertexAttribArray(uvLocation);
 	glVertexAttribPointer(uvLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-	stride = width;
-	const auto size = stride * height * 3;
-	pixels = new unsigned char[size];
-	for (int i = 0; i < height; ++i) {
-		for (int j = 0; j < stride; ++j) {
-			setPixel(j, i, 0, 0, 0);
-		}
-	}
+	const auto bytesPerPixel = 3;
+	const auto size = width * height * bytesPerPixel;
+	const auto pixels = new unsigned char[size];
+	auto pixelsDirty = true;
+	const Context drawContext = { pixels, width, bytesPerPixel };
 
 	unsigned int texture;
 	const auto textureState = 0;
@@ -160,31 +189,40 @@ int main(void) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, stride, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
-	glUniform1i(pixelsLocation, 0);	
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+	glUniform1i(pixelsLocation, 0);
 
 	auto previousTime = glfwGetTime();
+	auto angle = 0.f;
 	while (!glfwWindowShouldClose(window)) {
 		//glClear(GL_COLOR_BUFFER_BIT);
+		memset(pixels, 0, size);
 
 		auto currentTime = glfwGetTime();
 		const auto deltaTime = currentTime - previousTime;
-		previousTime = currentTime;		
+		previousTime = currentTime;
 
-		for (int i = 0; i < height; ++i) {
-			for (int j = 0; j < stride; ++j) {
-				setPixel(
-					j,
-					i,
-					(unsigned char)((((float)j) / stride) * 255.f),
-					(unsigned char)(((float)i / height) * 255.f),
-					(unsigned char)(((float)(stride - j) / stride) * 255.f)
-				);
-			}
-		}
+		//for (int i = 0; i < height; ++i) {
+		//	for (int j = 0; j < width; ++j) {
+		//		setPixel(
+		//			drawContext,
+		//			j,
+		//			i,
+		//			{ (unsigned char)((((float)j) / width) * 255.f),
+		//			(unsigned char)(((float)i / height) * 255.f),
+		//			(unsigned char)(((float)(width - j) / width) * 255.f) }
+		//		);
+		//	}
+		//}
+
+		auto x = width / 2;
+		auto y = height / 2;
+		line(drawContext, x, y, x + 100.f * sin(angle * 90.f * 0.0174533f), y + 100, { 255, 0, 0 });
+		angle += deltaTime;
+		pixelsDirty = true;
 
 		if (pixelsDirty) {
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, stride, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 			pixelsDirty = false;
 		}
 
