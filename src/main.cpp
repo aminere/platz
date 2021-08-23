9fd2b865651181a82eaef5bccc8b3f17c5566bf1
Amine Rehioui <amine@franticsoftware.com>
2021-08-24 00:03:03 +0100

    Rasterize a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+#include <glm/geometric.hpp>
 
 #include <iostream>
 
@@ -13,8 +15,50 @@ struct Color {
 
 struct Context {
 	unsigned char* pixels;
-	int pixelsStride;
+	int width;
+	int height;
 	int bytesPerPixel;
+};
+
+struct Triangle {
+	glm::vec3 a;
+	glm::vec3 b;
+	glm::vec3 c;
+
+	bool contains(const glm::vec3& p, glm::vec3& coords) {
+		auto _coords = getBarycentricCoords(p);
+		auto tolerance = 0.0005f;
+		auto inside = (_coords.y >= -tolerance)
+			&& (_coords.z >= -tolerance)
+			&& (_coords.y + _coords.z - tolerance <= 1.f);
+		
+		if (inside) {
+			coords = _coords;
+		}
+
+		return inside;
+	}
+
+	glm::vec3 getBarycentricCoords(const glm::vec3& p) {		
+		auto v0 = b - a;
+		auto v1 = c - a;
+		auto v2 = p - a;
+		auto d00 = glm::dot(v0, v0);
+		auto d01 = glm::dot(v0, v1);
+		auto d11 = glm::dot(v1, v1);
+		auto d20 = glm::dot(v2, v0);
+		auto d21 = glm::dot(v2, v1);
+		auto det = (d00 * d11 - d01 * d01);
+		if (det != 0) {
+			auto v = (d11 * d20 - d01 * d21) / det;
+			auto w = (d00 * d21 - d01 * d20) / det;
+			auto u = 1.0f - v - w;
+			return glm::vec3(u, v, w);
+		}
+		else {
+			return glm::vec3(0.f, 999999.f, 999999.f);
+		}
+	}
 };
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
@@ -22,18 +66,26 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
+Context drawContext;
 void resizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
+
+	const auto size = width * height * drawContext.bytesPerPixel;
+	const auto pixels = new unsigned char[size];
+	drawContext.width = width;
+	drawContext.height = height;
+	drawContext.pixels = pixels;
 }
 
-void setPixel(const Context& context, unsigned int x, unsigned int y, const Color& color) {
-	const auto index = (y * context.pixelsStride * context.bytesPerPixel) + x * context.bytesPerPixel;
+void drawPixel(const Context& context, unsigned int x, unsigned int y, const Color& color) {
+	const auto stride = context.width * context.bytesPerPixel;
+	const auto index = (y * stride) + x * context.bytesPerPixel;
 	context.pixels[index + 0] = color.r;
 	context.pixels[index + 1] = color.g;
 	context.pixels[index + 2] = color.b;	
 }
 
-void line(const Context& context, int x0, int y0, int x1, int y1, const Color& color) {
+void drawLine(const Context& context, float x0, float y0, float x1, float y1, const Color& color) {
 	// Based on https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 	const auto dx = abs(x1 - x0);
 	const auto sx = x0 < x1 ? 1 : -1;
@@ -41,7 +93,7 @@ void line(const Context& context, int x0, int y0, int x1, int y1, const Color& c
 	const auto sy = y0 < y1 ? 1 : -1;
 	auto err = dx + dy;
 	while (true) {
-		setPixel(context, x0, y0, color);
+		drawPixel(context, (unsigned int)x0, (unsigned int)y0, color);
 		if (x0 == x1 && y0 == y1) {
 			break;
 		}
@@ -53,6 +105,33 @@ void line(const Context& context, int x0, int y0, int x1, int y1, const Color& c
 		if (e2 <= dx) {
 			err += dx;
 			y0 += sy;
+		}
+	}
+}
+
+void drawLine(const Context& context, const glm::vec3& a, const glm::vec3& b, const Color& color) {
+	drawLine(context, a.x, a.y, b.x, b.y, color);
+}
+
+Color colorA = { 0xff, 0, 0 };
+Color colorB = { 0, 0xff, 0 };
+Color colorC = { 0, 0, 0xff };
+void drawTriangle(const Context& context, const glm::vec3& a, const glm::vec3& b, const glm::vec3& c, const Color& color) {
+	const auto minX = fminf(a.x, fminf(b.x, c.x));
+	const auto minY = fminf(a.y, fminf(b.y, c.y));
+	const auto maxX = fmaxf(a.x, fmaxf(b.x, c.x));
+	const auto maxY = fmaxf(a.y, fmaxf(b.y, c.y));
+	glm::vec3 coords;
+	Triangle triangle = { a, b, c };
+	for (auto i = minY; i < maxY; ++i) {
+		for (auto j = minY; j < maxY; ++j) {
+			if (triangle.contains(glm::vec3(j, i, 0), coords)) {
+				auto r = glm::min(coords.x * colorA.r + coords.y * colorB.r + coords.z * colorC.r, (float)0xff);
+				auto g = glm::min(coords.x * colorA.g + coords.y * colorB.g + coords.z * colorC.g, (float)0xff);
+				auto b = glm::min(coords.x * colorA.b + coords.y * colorB.b + coords.z * colorC.b, (float)0xff);				
+				Color c = { (unsigned int)r, (unsigned int)g, (unsigned int)b };
+				drawPixel(context, (unsigned int)j, (unsigned int)i, c);
+			}
 		}
 	}
 }
@@ -80,9 +159,8 @@ int main(void) {
 		return -1;
 	}
 
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);	
+	glfwGetFramebufferSize(window, &drawContext.width, &drawContext.height);
+	glViewport(0, 0, drawContext.width, drawContext.height);
 	glClearColor(0.f, 0.f, 0.f, 1.0f);
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
@@ -174,11 +252,9 @@ int main(void) {
 	glEnableVertexAttribArray(uvLocation);
 	glVertexAttribPointer(uvLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-	const auto bytesPerPixel = 3;
-	const auto size = width * height * bytesPerPixel;
-	const auto pixels = new unsigned char[size];
+	drawContext.bytesPerPixel = 3;
+	drawContext.pixels = new unsigned char[drawContext.width * drawContext.height * drawContext.bytesPerPixel];
 	auto pixelsDirty = true;
-	const Context drawContext = { pixels, width, bytesPerPixel };
 
 	unsigned int texture;
 	const auto textureState = 0;
@@ -189,14 +265,19 @@ int main(void) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, drawContext.width, drawContext.height, 0, GL_RGB, GL_UNSIGNED_BYTE, drawContext.pixels);
 	glUniform1i(pixelsLocation, 0);
 
+	glm::vec3 a(100, 100, 0);
+	glm::vec3 b(200, 100, 0);
+	glm::vec3 c(100, 200, 0);
+	Color color = { 0xff, 0xff, 0xff };
+
 	auto previousTime = glfwGetTime();
-	auto angle = 0.f;
 	while (!glfwWindowShouldClose(window)) {
-		//glClear(GL_COLOR_BUFFER_BIT);
-		memset(pixels, 0, size);
+
+		const auto size = drawContext.width * drawContext.height * drawContext.bytesPerPixel;
+		memset(drawContext.pixels, 0, size);
 
 		auto currentTime = glfwGetTime();
 		const auto deltaTime = currentTime - previousTime;
@@ -215,19 +296,19 @@ int main(void) {
 		//	}
 		//}
 
-		auto x = width / 2;
-		auto y = height / 2;
-		line(drawContext, x, y, x + 100.f * sin(angle * 90.f * 0.0174533f), y + 100, { 255, 0, 0 });
-		angle += deltaTime;
+		drawLine(drawContext, a, b, color);
+		drawLine(drawContext, b, c, color);
+		drawLine(drawContext, a, c, color);
+		drawTriangle(drawContext, a, b, c, color);
+		
 		pixelsDirty = true;
 
 		if (pixelsDirty) {
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, drawContext.width, drawContext.height, 0, GL_RGB, GL_UNSIGNED_BYTE, drawContext.pixels);
 			pixelsDirty = false;
 		}
 
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
